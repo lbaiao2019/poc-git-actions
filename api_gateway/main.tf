9b0723525a0978122e1393a40aa89f9e34859a32
Leonardo Baiao <lbaiao@morneaushepell.com>
2021-01-29 20:49:03 +0000

    fixed api-gateway

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -37,8 +37,6 @@ resource "aws_api_gateway_integration" "default" {
 resource "aws_api_gateway_deployment" "default" {
   rest_api_id       = aws_api_gateway_rest_api.default.id
   stage_name        = var.stage_name
-  stage_description = "Deployed at ${timestamp()}"
-
   depends_on = [aws_api_gateway_method.default, aws_api_gateway_integration.default]
 }
 resource "aws_api_gateway_usage_plan" "default" {
